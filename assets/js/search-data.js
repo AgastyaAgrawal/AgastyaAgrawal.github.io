5c53bb6decdc98316240ed36f4fe66d55715215f
AgastyaAgrawal <agastya.agr@gmail.com>
2026-05-16 02:47:12 +0000

    Deploying to gh-pages from @ AgastyaAgrawal/AgastyaAgrawal.github.io@01ba90d299086b789e489493dd60856cc07d04b2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -105,9 +105,9 @@ ninja.data = [{
           description: "A statistical learning perspective on reinforcement learning.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_rl_phase/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-singular-learning-theory",
+          title: 'Singular Learning Theory',
+          description: "I am not satisfied with any of the material on SLT online, so I decided to write my own. I have made an attempt to make it very comprehensive and very self contained.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
